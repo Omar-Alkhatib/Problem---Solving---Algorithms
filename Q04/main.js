7244f91beecd90192515f32829462ae5e29a6fe1
unknown <omar.q.alkhatib@gmail.comgit config --global user.name Omar-Alkhatib git config --global user.email "omar.q.alkhatib@gmail.comgit config --global user.name "Omar-Alkhatib>
2020-10-02 16:55:55 +0300

    Q04 Solution

--- Q04/main.js
+++ Q04/main.js
@@ -3,7 +3,8 @@ console.log('Problem Solving Q: 4 ');
 /* Codeland Username Validation */
 
 /*  
-Have the function CodelandUsernameValidation(str) take the str parameter being passed and determine if the string is a valid username according to the following rules:
+Have the function CodelandUsernameValidation(str) take the str parameter being passed and determine if 
+the string is a valid username according to the following rules:
 
 1. The username is between 4 and 25 characters.
 2. It must start with a letter.
@@ -15,6 +16,33 @@ If the username is valid then your program should return the string true, otherw
 
 function CodelandUsernameValidation(str) {
   // YOUR CODE HERE
+  let stringsNotAllowable = '!"#$%&\'()*+,- ./:;<=>?@[\\]^`{|}~';
+  let numbersNotAllowable = '1234567890';
+  if (str.length > 4 && str.length < 25) {
+    console.log("its between 4 and 24")
+  if (stringsNotAllowable.indexOf(str[0]) === -1 && str[0] !== "_" && numbersNotAllowable.indexOf(str[0]) === -1) {
+    console.log("it starts with a letter " + str[0])
+    if (str[str.length -1] !== "_" ) {
+      console.log("it doesnt end with a _")
+      result = true
+      let stringArray = str.split("");
+      console.log(stringArray)
+
+      stringArray.forEach (function (elem, ind){
+          if (stringsNotAllowable.indexOf(elem) !== -1) {
+            console.log("it contains " +elem)
+            result = false;
+            return result
+          }
+         
+      })
+      return result;
+    }
+    return false
+  }
+  return false
+}
+return false
 }
 
 /* 
