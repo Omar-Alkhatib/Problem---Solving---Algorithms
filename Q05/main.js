2cfe168c9e09d37b6070e550ac09a7bccaf2c523
unknown <omar.q.alkhatib@gmail.comgit config --global user.name Omar-Alkhatib git config --global user.email "omar.q.alkhatib@gmail.comgit config --global user.name "Omar-Alkhatib>
2020-10-03 13:21:15 +0300

    Q05 solution

--- Q05/main.js
+++ Q05/main.js
@@ -3,11 +3,31 @@ console.log('Problem Solving Q: 5 ');
 /* Find Intersection */
 
 /*  
-Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 
+elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the 
+second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return 
+a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is 
+no intersection, return the string false.
 */
 
 function FindIntersection(strArr) {
   // YOUR CODE HERE
+
+  let firstString = strArr[0].split(",").join("").split(" ").join(",");
+  let secondStringArr = strArr[1].split(",").join("").split(" ");
+  // console.log(firstString);
+  // console.log(secondStringArr);
+
+  const resultArr = secondStringArr.filter(function (elem, ind) {
+
+    return firstString.indexOf(elem) !== -1;
+    
+  })
+
+  if (resultArr.length === 0){
+    return false;
+  }
+  else { return resultArr.join(","); }
   
 }
 
